5acc127209e70eb454d45437a4952a7cb6a11002
crc64error <40744269+crc64error@users.noreply.github.com>
2020-11-06 11:50:23 -0600

    n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: '<h1>My App</h1>'
+  selector: 'mw-app',
+  templateUrl: './app.component.html',
+//   styles: [`
+//         h1 {
+//             color: rgba(255,255,255,1);
+//         }
+//         .description {
+//             font-style: italic;
+//             color: rgba(45,255,45,1);
+//         }
+//       `]
+
+styleUrls: [
+    './app.component.css'
+]
 })
 export class AppComponent {}
